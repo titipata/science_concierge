ff442814478a861c87ce6edf3014f6075f626204
Titipat Achakulvisut <titipat.a@u.northwestern.edu>
2016-05-05 01:51:27 -0500

    Fix license in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         author = "Titipat Achakulvisut, Daniel Acuna, Tulakan Ruangrong",
         author_email = "titipat.a@u.northwestern.edu",
         description = ("a Python repository implementing Rocchio algorithm content-based suggestion based on topic distance space using Latent semantic analysis (LSA)"),
-        license = "MIT",
+        license = "Creative Common 4.0",
         keywords = "Python library that implements a fast and accurate recommendation system for literature search",
         url = "http://packages.python.org/an_example_pypi_project",
         long_description=read('README.md'),
