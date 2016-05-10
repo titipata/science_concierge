ac82b5e1e57c97c9ad2cffe20b13a011fa2f5f92
titipata <titipat.a@u.northwestern.edu>
2016-05-10 15:53:22 -0500

    csv file for plot and R code

--- article/human_vs_topic_distance.R
+++ article/human_vs_topic_distance.R
@@ -9,17 +9,24 @@ mean_point <- stat_summary(fun.y = 'mean', geom = 'point', position = position_d
 mean_line <- stat_summary(fun.y = 'mean', geom = 'line', position = position_dodge(width = 0.0))
 se_error <- stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.05, position = position_dodge(width = 0.0))
 
-experiments <- read.csv('data//human_vs_topic_distance.csv')
-experiments$abstact_distance <- scale(experiments$abstact_distance)
+experiments <- read.csv('human_vs_topic_distance_entropy.csv')
+experiments$tfidf_distance <- scale(experiments$tfidf_distance)
 experiments$keyword_distance <- scale(experiments$keyword_distance)
+experiments$wordvec_distance <- scale(experiments$wordvec_distance)
+experiments$countvec_distance <- scale(experiments$countvec_distance)
+experiments$logentropy_distance <- scale(experiments$logentropy_distance)
 experiments_melt <- melt(experiments, id.vars = c('poster_number', 'human_distance'))
 
 experiments_melt$variable <- revalue(experiments_melt$variable,
-                                    c("abstact_distance"="Science Concierge",
-                                      "keyword_distance"="Keyword"))
+                                    c("tfidf_distance"="tf-idf",
+                                      "keyword_distance"="keyword",
+                                      "countvec_distance"="term frequency",
+                                      "logentropy_distance"="log-entropy",
+                                      "wordvec_distance"="average word vector"))
 
 experiments_melt$variable <- factor(experiments_melt$variable,
-                                    c('Keyword', 'Science Concierge'))
+                                   c('term frequency', 'tf-idf', 'log-entropy',
+                                     'average word vector', 'keyword'))
 
 pdf('figures/human_vs_topic_distance.pdf', width=6, height=3)
 ggplot(experiments_melt, aes(x = human_distance, y = value, color=variable))  +
@@ -28,9 +35,10 @@ ggplot(experiments_melt, aes(x = human_distance, y = value, color=variable))  +
   se_error +
   scale_color_brewer(palette = 6, type = 'qual', name = 'Algorithm') +
   ylab('Topic distance (z-score)') +
-  xlab('Human Curated Distance') +
+  xlab('Human curated topic distance') +
   theme_classic()
 dev.off()
+pdf('figures/human_vs_topic_distance.pdf', width=6, height=3)
 
 # Spearman correlation
 cor.test(experiments$human_distance, experiments$abstact_distance, method = "spearman")
--- article/multiple_votes.R
+++ article/multiple_votes.R
@@ -6,9 +6,9 @@ library(grid)
 library(plyr)
 
 plot_theme <- theme_classic() +
-  theme(text = element_text(size = 20), 
-        axis.title.x = element_text(vjust = -1, face = 'bold'), 
-        axis.title.y = element_text(vjust = 2, face = 'bold'), 
+  theme(text = element_text(size = 20),
+        axis.title.x = element_text(vjust = -1, face = 'bold'),
+        axis.title.y = element_text(vjust = 2, face = 'bold'),
         plot.margin =  (unit(c(2, 2, 2, 2), "cm")),
         legend.position = c(1, 0), legend.justification = c(1, 0))
 
@@ -17,27 +17,33 @@ mean_point <- stat_summary(fun.y = 'mean', geom = 'point', position = position_d
 mean_line <- stat_summary(fun.y = 'mean', geom = 'line', position = position_dodge(width = 0.1))
 se_error <- stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.05, position = position_dodge(width = 0.1))
 
-experiments <- melt(read.csv('data//poster_node_distance.csv'),
+experiments <- melt(read.csv('data//multiple_votes.csv'),
                    id.vars = c('poster_number', 'number_recommend'))
 
-experiments$variable <- revalue(experiments$variable, 
-                                c("avg_node_distance"="Science Concierge",
-                                "avg_node_distance_kw"="Keyword",
-                                "avg_random"="Random"))
+experiments$variable <- revalue(experiments$variable,
+                                c("avg_node_distance"="tf-idf",
+                                "avg_node_distance_kw"="keyword",
+                                "avg_node_distance_wv"="average word vector",
+                                "avg_node_distance_cv"="term frequency",
+                                "avg_node_distance_le"="log-entropy",
+                                "avg_random"="random"))
 
-experiments$variable <- factor(experiments$variable, 
-                               c('Random', 'Keyword', 'Science Concierge'))
+experiments$variable <- factor(experiments$variable,
+                               c('term frequency', 'tf-idf', 'log-entropy',
+                                 'average word vector', 'keyword'))
+
 
 pdf('figures/performance_vs_votes.pdf', width=6, height=3)
-ggplot(experiments, 
-       aes(x = number_recommend, y = value, color = variable)) + 
-  mean_point + 
-  mean_line + 
-  se_error + 
+ggplot(experiments,
+       aes(x = number_recommend, y = value, color = variable)) +
+  mean_point +
+  mean_line +
+  se_error +
   theme_classic() +
-  scale_y_continuous(breaks = c(1, 2, 3)) + 
+  scale_y_continuous(breaks = c(1, 2, 3)) +
+  scale_x_continuous(breaks = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) +
   coord_cartesian(ylim = c(0, 3)) +
-  scale_color_brewer(palette = 6, type = 'qual', name = 'Algorithm') + 
-  ylab('Topic distance') +
-  xlab('Number of votes')  
+  scale_color_brewer(palette = 6, type = 'qual', name = 'Algorithm') +
+  ylab('Average human curated topic distance') +
+  xlab('Number of votes')
 dev.off()
