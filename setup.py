cf734fc64217d46db65b945f3742f977f97e1513
Titipat Achakulvisut <titipat.a@u.northwestern.edu>
2016-05-13 03:20:10 -0500

    update url in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 #! /usr/bin/env python
 import os
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+DESCRIPTION = """
+    a Python repository implementing Rocchio algorithm content-based suggestion 
+    based on topic distance space using Latent semantic analysis (LSA)
+    """
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -11,10 +19,10 @@ if __name__ == "__main__":
         version = "0.1",
         author = "Titipat Achakulvisut, Daniel Acuna, Tulakan Ruangrong",
         author_email = "titipat.a@u.northwestern.edu",
-        description = ("a Python repository implementing Rocchio algorithm content-based suggestion based on topic distance space using Latent semantic analysis (LSA)"),
+        description = DESCRIPTION,
         license = "Creative Common 4.0",
-        keywords = "Python library that implements a fast and accurate recommendation system for literature search",
-        url = "http://packages.python.org/an_example_pypi_project",
+        keywords = "recommendation system, Latent Semantic Analysis",
+        url = "https://github.com/titipata/science_concierge",
         long_description=read('README.md'),
         packages=['science_concierge'],
     )
